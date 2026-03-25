f405ed1effa8f795df5cff53874ce28925145808
ayoub-l-l <ayela006@osloskolen.no>
2026-03-25 14:25:09 +0100

    1

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function createNote() {
-  // hent tekst fra input
+  
   let input = document.getElementById("noteInput");
   let text = input.value;
 
-  if (text === "") return; // ikke legg til tomt
+  if (text === "") return; 
 
-  // lag et nytt listeelement
   let li = document.createElement("li");
   li.textContent = text;
 
